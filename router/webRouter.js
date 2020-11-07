fd31dff57905fe19701708f36c49adf6e766a5db
pmh-only <pmhstudio.pmh@gmail.com>
2020-11-07 22:03:58 +0900

    disable ad

--- router/webRouter.js
+++ router/webRouter.js
@@ -6,17 +6,11 @@ const { renderFile: render } = require('ejs')
  * @param {import('knex')} db
  */
 module.exports = (app, db) => {
-  app.get('/',         async (  _, res) => res.redirect('/edit/-'))
-  app.get('/edit/:id', async (req, res) => {
-    let [ embed ] = await db.select('*').where('id', req.params.id).from('embeds')
-    if (!embed) embed = require('../presets/notfound.json')
-    const html = await render(path + '/page/editor.ejs', { embed }, { async: true })
-    res.send(html)
-  })
+  app.get('/',    async (  _, res) => res.redirect('/-'))
   app.get('/:id', async (req, res) => {
     let [ embed ] = await db.select('*').where('id', req.params.id).from('embeds')
     if (!embed) embed = require('../presets/notfound.json')
-    const html = await render(path + '/page/embed.ejs', { embed, id: req.params.id }, { async: true })
+    const html = await render(path + '/page/index.ejs', { embed }, { async: true })
     res.send(html)
   })
 }
